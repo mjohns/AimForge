0f7d21fa5ad551294cb6db444c64142b45d4632d
Michael Johns <1633607+mjohns@users.noreply.github.com>
2025-01-25 18:56:32 -0500

    Add ability to use flatbuffer. Will store settings and replays.

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,18 @@
 #include "application.h"
 #include "model.h"
+#include "flatbuffers/flatbuffers.h"
+#include "replay_generated.h"
 
 int main(int, char**) {
   auto app = aim::Application::Create();
 
+  flatbuffers::FlatBufferBuilder builder;
+  auto replay = CreateReplayDirect(builder, "Abominable Snowman", 100);
+  builder.Finish(replay);
+
   aim::Scenario s;
   s.Run(app.get());
+
 
   return 0;
 }
