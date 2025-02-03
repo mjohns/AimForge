4e63e6ff4af1386d729f300deacbf3c0e492d576
Michael Johns <michael.r.johns@gmail.com>
2025-02-02 19:06:22 -0500

    Display the high score on the stats screen

--- src/aim/database/stats_db.h
+++ src/aim/database/stats_db.h
@@ -1,16 +1,18 @@
 #pragma once
 
 #include <sqlite3.h>
+
 #include <string>
+#include <vector>
 
 namespace aim {
 
 struct StatsRow {
   int64_t stats_id = 0;
   std::string timestamp;
-  uint32_t num_hits = 0;
-  uint32_t num_kills = 0;
-  uint32_t num_shots = 0;
+  int64_t num_hits = 0;
+  int64_t num_kills = 0;
+  int64_t num_shots = 0;
   double score = 0.0;
 };
 
@@ -21,6 +23,8 @@ class StatsDb {
 
   void AddStats(const std::string& scenario_id, StatsRow* row);
 
+  std::vector<StatsRow> GetStats(const std::string& scenario_id);
+
   StatsDb(const StatsDb&) = delete;
   StatsDb(StatsDb&&) = default;
   StatsDb& operator=(StatsDb other) = delete;
@@ -30,4 +34,4 @@ class StatsDb {
   sqlite3* db_ = nullptr;
 };
 
-} // namespace aim
+}  // namespace aim
