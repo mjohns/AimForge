37903b750d42d381ea8b00a6a01df82bdf8ec71f
Michael Johns <michael.r.johns@gmail.com>
2025-02-03 09:29:31 -0500

    Add raw control scenario type

--- src/aim/scenario/static_scenario.h
+++ src/aim/scenario/static_scenario.h
@@ -6,6 +6,27 @@
 
 namespace aim {
 
+struct TargetRegion {
+  // The percent chance the target will be placed in this region.
+  float percent_chance = 1.0f;
+
+  // Specify one of the subcategories.
+
+  // The allowed percent of the wall in each dimension where targets are allowed to be placed.
+  float x_percent = -1;
+  float y_percent = -1;
+
+  // Circle/Oval within the specified percent o x/y
+  float x_circle_percent = -1;
+  float y_circle_percent = -1;
+};
+
+struct TargetPlacementParams {
+  // Target is placed in first region where the percent_chance roll "hits".
+  std::vector<TargetRegion> regions;
+  float min_distance = 20;
+};
+
 struct StaticScenarioParams {
   std::string scenario_id;
   int num_targets = 1;
@@ -14,6 +35,7 @@ struct StaticScenarioParams {
   float target_radius = 2;
   float duration_seconds = 60;
   float cm_per_360 = 45;
+  TargetPlacementParams target_placement;
 };
 
 class StaticScenario {
