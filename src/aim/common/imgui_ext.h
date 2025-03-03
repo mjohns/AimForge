1dd82640d7d62aebf89236061dce0eecdcc56a7d
Michael Johns <1633607+mjohns@users.noreply.github.com>
2025-03-02 20:32:30 -0500

    Remove scoped style which is not working correctly

--- src/aim/common/imgui_ext.h
+++ src/aim/common/imgui_ext.h
@@ -5,37 +5,6 @@
 #include <format>
 #include <string>
 
-namespace aim {
-
-class ScopedStyleColor {
- public:
-  ScopedStyleColor(ImGuiCol idx, ImU32 col) {
-    ImGui::PushStyleColor(idx, col);
-  }
-
-  static ScopedStyleColor Text(ImU32 col) {
-    return ScopedStyleColor(ImGuiCol_Text, col);
-  }
-
-  ~ScopedStyleColor() {
-    End();
-  }
-
-  void End() {
-    if (!closed_) {
-      closed_ = true;
-      ImGui::PopStyleColor();
-    }
-  }
-
- private:
-  ImGuiCol idx_;
-  ImU32 col_;
-  bool closed_ = false;
-};
-
-}  // namespace aim
-
 namespace ImGui {
 
 // ImGui::Text but taking same arguments as std::format to product the text.
