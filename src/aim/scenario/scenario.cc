3e73ac94b18d8da64789f1e7c62886bc7f9ed139
Michael Johns <michael.r.johns@gmail.com>
2025-03-02 20:32:30 -0500

    Remove scoped style which is not working correctly

--- src/aim/scenario/scenario.cc
+++ src/aim/scenario/scenario.cc
@@ -170,7 +170,7 @@ NavigationEvent Scenario::RunWaitingScreenAndThenStart() {
     ImGui::Text("theme: %s", settings_.theme_name().c_str());
     ImGui::Text("cm/360: %.0f", settings_.cm_per_360());
 
-    auto text_color = ScopedStyleColor::Text(ToImCol32(theme_.target_color()));
+    ImGui::PushStyleColor(ImGuiCol_Text, ToImCol32(theme_.target_color()));
     {
       auto bold = app_->font_manager()->UseLargeBold();
       std::string message = "Click to Start";
@@ -191,8 +191,7 @@ NavigationEvent Scenario::RunWaitingScreenAndThenStart() {
     ImGui::SetCursorPosY(app_->screen_info().center.y + text_size.y * 2);
     ImGui::Text("%s", message.c_str());
 
-    text_color.End();
-
+    ImGui::PopStyleColor();
     ImGui::End();
 
     if (app_->StartRender(ImVec4(0, 0, 0, 1))) {
