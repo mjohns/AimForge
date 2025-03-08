97f3f7cf4a452abab9eb4f4fb0457d262dda64ef
Michael Johns <michael.r.johns@gmail.com>
2025-03-08 14:51:26 -0500

    Remove old opengl renderers

--- src/aim/graphics/glad_loader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace aim {
-
-// Calls gladLoadGLLoader. This hides glad.h in a separate compilation unit to prevent conflicts
-// over headers.
-int LoadGlad();
-
-}  // namespace aim
