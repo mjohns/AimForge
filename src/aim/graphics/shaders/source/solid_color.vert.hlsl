c06634afc1c7ea0baba338b3fe9a2f6efb43f88f
Michael Johns <1633607+mjohns@users.noreply.github.com>
2025-03-07 19:53:52 -0500

    Color as uniform

--- src/aim/graphics/shaders/source/solid_color.vert.hlsl
+++ src/aim/graphics/shaders/source/solid_color.vert.hlsl
@@ -1,3 +1,8 @@
+cbuffer UniformBlock : register(b0, space1)
+{
+    float4x4 MatrixTransform : packoffset(c0);
+};
+
 struct Input
 {
     float3 Position : POSITION0;
@@ -6,15 +11,12 @@ struct Input
 
 struct Output
 {
-    float4 Color : COLOR;
     float4 Position : SV_Position;
 };
 
 Output main(Input input)
 {
     Output output;
-    output.Color = float4(1.0f, 1.0f, 0.0f, 1.0f);
-    output.Position = float4(input.Position, 1.0f);
-    //output.Position = mul(input.Transform, float4(input.Position, 1.0f));
+    output.Position = mul(MatrixTransform, float4(input.Position, 1.0f));
     return output;
 }
