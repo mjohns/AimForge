5ee16787732053c85a20ca6b9910ee7b99ff1576
Michael Johns <1633607+mjohns@users.noreply.github.com>
2025-03-08 15:11:37 -0500

    Add historgrams for performance stats

--- src/aim/core/perf.h
+++ src/aim/core/perf.h
@@ -22,4 +22,46 @@ struct FrameTimes {
   u64 frame_number = 0;
 };
 
-} // namespace aim
+struct TimeHistogram {
+  u64 bucket_100 = 0;
+  u64 bucket_300 = 0;
+  u64 bucket_500 = 0;
+  u64 bucket_700 = 0;
+  u64 bucket_1000 = 0;
+  u64 bucket_1500 = 0;
+  u64 bucket_2000 = 0;
+  u64 bucket_3000 = 0;
+  u64 bucket_5000 = 0;
+
+  void Increment(u64 value) {
+    if (value <= 0) {
+      return;
+    } else if (value < 100) {
+      bucket_100++;
+    } else if (value < 300) {
+      bucket_300++;
+    } else if (value < 500) {
+      bucket_500++;
+    } else if (value < 700) {
+      bucket_700++;
+    } else if (value < 1000) {
+      bucket_1000++;
+    } else if (value < 1500) {
+      bucket_1500++;
+    } else if (value < 2000) {
+      bucket_2000++;
+    } else if (value < 3000) {
+      bucket_3000++;
+    } else {
+      bucket_5000++;
+    }
+  }
+};
+
+struct RunPerformanceStats {
+  FrameTimes worst_times{};
+  TimeHistogram total_time_histogram{};
+  TimeHistogram render_time_histogram{};
+};
+
+}  // namespace aim
