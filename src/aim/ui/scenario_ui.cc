f388daf0114d4a170b3c9a36d3bab800506aa7c3
Michael Johns <michael.r.johns@gmail.com>
2025-05-21 17:48:43 -0400

    Delete confirmation dialog

--- src/aim/ui/scenario_ui.cc
+++ src/aim/ui/scenario_ui.cc
@@ -10,6 +10,8 @@
 namespace aim {
 namespace {
 
+const char* kDeleteConfirmationPopup = "DELETE_CONFIRMATION_POPUP";
+
 std::vector<std::string> GetAllRelativeNamesInBundle(const std::string& bundle_name,
                                                      Application* app) {
   std::vector<std::string> names;
@@ -28,6 +30,8 @@ class ScenarioBrowserComponentImpl : public UiComponent, public ScenarioBrowserC
   void Show(ScenarioBrowserResult* result) override {
     auto cid = GetComponentIdGuard();
 
+    DrawDeleteConfirmationPopup(result);
+
     ImVec2 char_size = ImGui::CalcTextSize("A");
     ImGui::SetNextItemWidth(char_size.x * 30);
     ImGui::InputTextWithHint("##ScenarioSearchInput", "Search..", &search_text_);
@@ -43,6 +47,43 @@ class ScenarioBrowserComponentImpl : public UiComponent, public ScenarioBrowserC
   }
 
  private:
+  void DrawDeleteConfirmationPopup(ScenarioBrowserResult* result) {
+    bool show_popup = scenario_to_maybe_delete_.size() > 0;
+    if (show_popup) {
+      ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(),
+                              ImGuiCond_Appearing,
+                              ImVec2(0.5f, 0.5f));  // Center the popup
+      if (ImGui::BeginPopupModal(kDeleteConfirmationPopup,
+                                 &show_popup,
+                                 ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove |
+                                     ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar)) {
+        ImGui::TextFmt("Delete \"{}\"?", scenario_to_maybe_delete_);
+
+        float button_width = ImGui::CalcTextSize("OK").x + ImGui::GetStyle().FramePadding.x * 2.0f;
+        ImGui::SetCursorPosX((ImGui::GetWindowSize().x - button_width) * 0.5f);
+
+        if (ImGui::Button("Delete")) {
+          auto maybe_scenario = app_->scenario_manager()->GetScenario(scenario_to_maybe_delete_);
+          if (maybe_scenario.has_value()) {
+            app_->scenario_manager()->DeleteScenario(maybe_scenario->name);
+            result->reload_scenarios = true;
+          }
+          scenario_to_maybe_delete_ = "";
+          ImGui::CloseCurrentPopup();
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Cancel")) {
+          ImGui::CloseCurrentPopup();
+        }
+        ImGui::EndPopup();
+      }
+    }
+    if (open_delete_confirmation_popup_) {
+      ImGui::OpenPopup(kDeleteConfirmationPopup);
+      open_delete_confirmation_popup_ = false;
+    }
+  }
+
   void CopyScenario(const ScenarioItem& item) {
     auto& mgr = *app_->scenario_manager();
     std::vector<std::string> taken_names =
@@ -78,8 +119,8 @@ class ScenarioBrowserComponentImpl : public UiComponent, public ScenarioBrowserC
             result->reload_scenarios = true;
           }
           if (ImGui::Selectable("Delete")) {
-            app_->scenario_manager()->DeleteScenario(node->scenario->name);
-            result->reload_scenarios = true;
+            scenario_to_maybe_delete_ = node->name;
+            open_delete_confirmation_popup_ = true;
           }
           /*
           if (ImGui::Selectable("Open file")) {
@@ -103,6 +144,9 @@ class ScenarioBrowserComponentImpl : public UiComponent, public ScenarioBrowserC
   }
 
   std::string search_text_;
+
+  bool open_delete_confirmation_popup_ = false;
+  std::string scenario_to_maybe_delete_;
 };
 
 }  // namespace
