7154a0ecc889441feb1c061cad26196fe3a9eeec
Michael Johns <michael.r.johns@gmail.com>
2025-06-01 22:35:49 -0400

    Recent playlist/scenario caching in history_manager

--- src/aim/core/history_manager.cc
+++ src/aim/core/history_manager.cc
@@ -1,13 +1,24 @@
 #include "history_manager.h"
 
-#include<memory>
+#include <memory>
 
 namespace aim {
+namespace {
+
+const int kCachedRecentNamesSize = 120;
+
+}  // namespace
 
 HistoryManager::HistoryManager(FileSystem* fs)
     : history_db_(std::make_unique<HistoryDb>(fs->GetUserDataPath("history.db"))) {}
 
 void HistoryManager::UpdateRecentView(RecentViewType t, const std::string& id) {
+  if (t == RecentViewType::SCENARIO) {
+    scenarios_need_reload_ = true;
+  }
+  if (t == RecentViewType::PLAYLIST) {
+    playlists_need_reload_ = true;
+  }
   return history_db_->UpdateRecentView(t, id);
 }
 
@@ -17,6 +28,30 @@ std::vector<RecentView> HistoryManager::GetRecentViews(RecentViewType t, int lim
 
 std::vector<std::string> HistoryManager::GetRecentUniqueNames(RecentViewType t, int limit) {
   return history_db_->GetRecentUniqueNames(t, limit);
+}
+
+void HistoryManager::MaybeReloadScenarios() {
+  if (scenarios_need_reload_) {
+    scenarios_need_reload_ = false;
+    recent_scenario_ids_ = GetRecentUniqueNames(RecentViewType::SCENARIO, kCachedRecentNamesSize);
+  }
+}
+
+void HistoryManager::MaybeReloadPlaylists() {
+  if (playlists_need_reload_) {
+    playlists_need_reload_ = false;
+    recent_playlists_ = GetRecentUniqueNames(RecentViewType::PLAYLIST, kCachedRecentNamesSize);
+  }
+}
+
+const std::vector<std::string>& HistoryManager::recent_scenario_ids() {
+  MaybeReloadScenarios();
+  return recent_scenario_ids_;
+}
+
+const std::vector<std::string>& HistoryManager::recent_playlists() {
+  MaybeReloadPlaylists();
+  return recent_playlists_;
 }
 
 }  // namespace aim
--- src/aim/core/history_manager.h
+++ src/aim/core/history_manager.h
@@ -3,6 +3,7 @@
 #include <optional>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 #include "aim/common/simple_types.h"
 #include "aim/core/file_system.h"
@@ -17,10 +18,22 @@ class HistoryManager {
 
   void UpdateRecentView(RecentViewType t, const std::string& id);
   std::vector<RecentView> GetRecentViews(RecentViewType t, int limit);
+
   std::vector<std::string> GetRecentUniqueNames(RecentViewType t, int limit);
 
+  const std::vector<std::string>& recent_scenario_ids();
+  const std::vector<std::string>& recent_playlists();
+
  private:
+  void MaybeReloadScenarios();
+  void MaybeReloadPlaylists();
+
   std::unique_ptr<HistoryDb> history_db_;
+
+  std::vector<std::string> recent_scenario_ids_;
+  std::vector<std::string> recent_playlists_;
+  bool scenarios_need_reload_ = true;
+  bool playlists_need_reload_ = true;
 };
 
 }  // namespace aim
