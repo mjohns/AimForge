b8d787e7a7a85bce1f61c5c3c910e098d22ae2a4
Michael Johns <1633607+mjohns@users.noreply.github.com>
2025-06-22 09:41:35 -0400

    Make sine bounce off walls

--- src/aim/scenario/types/sine_scenario.cc
+++ src/aim/scenario/types/sine_scenario.cc
@@ -27,10 +27,11 @@ class SineScenario : public BaseScenario {
  public:
   explicit SineScenario(const CreateScenarioParams& params, Application* app)
       : BaseScenario(params, app), wall_(Wall::ForRoom(params.def.room())) {
-    d_ = params.def.sine_def();
+    auto d = params.def.sine_def();
+    going_left_ = d.going_left();
 
-    height_ = wall_.GetRegionLength(d_.height());
-    float width = wall_.GetRegionLength(d_.width());
+    height_ = wall_.GetRegionLength(d.height());
+    float width = wall_.GetRegionLength(d.width());
 
     x_scale_ = width / glm::two_pi<float>();
     sine_length_ = GetSineLength(height_, x_scale_);
@@ -58,6 +59,22 @@ class SineScenario : public BaseScenario {
       return;
     }
 
+    float max_x = (wall_.width * 0.5) - (target->radius * 1.2);
+    float min_x = -1 * max_x;
+    float current_x = target->wall_position->x;
+    if (current_x >= max_x) {
+      going_left_ = true;
+      int pi_times = current_t_ / glm::two_pi<float>();
+      current_t_ -= pi_times * glm::two_pi<float>();
+      wall_start_.x = current_x + (x_scale_ * current_t_);
+    }
+    if (current_x <= min_x) {
+      going_left_ = false;
+      int pi_times = current_t_ / glm::two_pi<float>();
+      current_t_ -= pi_times * glm::two_pi<float>();
+      wall_start_.x = current_x - (x_scale_ * current_t_);
+    }
+
     float now_seconds = timer_.GetElapsedSeconds();
     float delta_seconds = now_seconds - last_update_time_;
     last_update_time_ = now_seconds;
@@ -65,7 +82,7 @@ class SineScenario : public BaseScenario {
     float next_t = current_t_ + (delta_seconds * t_speed_ * target->speed);
     current_t_ = next_t;
 
-    float direction_mult = d_.going_left() ? -1.0 : 1.0;
+    float direction_mult = going_left_ ? -1.0 : 1.0;
     glm::vec2 sine_point(direction_mult * x_scale_ * next_t, height_ * sin(next_t));
 
     auto point = wall_start_ + sine_point;
@@ -107,7 +124,8 @@ class SineScenario : public BaseScenario {
   float x_scale_;
   float sine_length_ = 0;
   float t_speed_ = 0;
-  SineScenarioDef d_;
+
+  bool going_left_ = false;
 };
 
 }  // namespace
