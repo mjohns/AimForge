a0bcb64262ea972a80f575ce578c89f3a64fd9f0
Michael Johns <1633607+mjohns@users.noreply.github.com>
2026-01-23 13:12:04 -0500

    Make sure initial run on new install works with DPI / layout

--- src/aim/core/settings_manager.cc
+++ src/aim/core/settings_manager.cc
@@ -23,11 +23,11 @@ constexpr const float kDefaultDpi = 800;
 
 Settings GetDefaultSettings() {
   Settings settings;
-  settings.set_dpi(kDefaultDpi);
   settings.set_cm_per_360(45);
   settings.set_current_crosshair_name("AF Dot");
   settings.set_crosshair_size(15);
   settings.mutable_health_bar()->set_show(true);
+  settings.set_theme_name("AF Concrete");
 
   Keybinds* binds = settings.mutable_keybinds();
   binds->mutable_fire()->set_mapping1("Left Click");
